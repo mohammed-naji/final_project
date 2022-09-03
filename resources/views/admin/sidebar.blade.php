b35a4e9d7a02e49e0b4af40af7e8bc02521c52b2
Mohammed Naji <m.naji1512@gmail.com>
2022-09-03 19:15:41 +0300

    admin

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,90 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon">
+        <i class="fas fa-store"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">{{ env('APP_NAME') }}</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>{{ __('admin.dashboard') }}</span></a>
+</li>
+
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-fw fa-tags"></i>
+        <span>{{ __('admin.categories') }}</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="buttons.html">All Categories</a>
+            <a class="collapse-item" href="cards.html">Add New</a>
+        </div>
+    </div>
+</li>
+
+<hr class="sidebar-divider my-0">
+
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-fw fa-heart"></i>
+        <span>{{ __('admin.products') }}</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+
+            <a class="collapse-item" href="buttons.html">All Products</a>
+            <a class="collapse-item" href="cards.html">Add New</a>
+        </div>
+    </div>
+</li>
+
+<hr class="sidebar-divider my-0">
+
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-shopping-cart"></i>
+        <span>{{ __('admin.orders') }}</span></a>
+</li>
+
+<hr class="sidebar-divider my-0">
+
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-money-bill"></i>
+        <span>{{ __('admin.payments') }}</span></a>
+</li>
+
+<hr class="sidebar-divider my-0">
+
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-users"></i>
+        <span>{{ __('admin.users') }}</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::prefix(LaravelLocalization::setLocale())->group(function() {
+    Route::prefix('admin')->name('admin.')->group(function() {
+        Route::get('/', [AdminController::class, 'index'])->name('index');
+    });
+});
